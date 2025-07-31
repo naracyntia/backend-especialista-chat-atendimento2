61d47a765d16a4d72567bdc667c8e7739cc7c0b9
rosanaandre <kawaiicri@gmail.com>
2025-07-31 18:57:16 -0500

    Criação do método POST

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
     @Get()
     async getAllUsers(){
         return JSON.stringify({ test: 'Chat Atendimento' });
+    }
+
+    @Post() 
+    async createUser(
+        @Body() createUser: CreateUserDto
+    ) {
+        return createUser;
     }
 }
