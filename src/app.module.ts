ec98cd3058823fd9ba80c54caf2f9819542da123
naracyntia <naracyntiam@gmail.com>
2025-08-14 20:35:32 -0300

    Configuracao do env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule],
+  
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
+
+
+
