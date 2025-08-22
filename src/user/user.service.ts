8fff56f20937d4292c78834a7400b9e8be01db72
rosanaandre <kawaiicri@gmail.com>
2025-08-21 19:19:16 -0500

    Criação do migration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,6 +20,7 @@ export class UserService {
         return this.userRepository.save({
             ...createUserDto,
             password:passwordHash,
+            confirmPassword:undefined,
 
         })    
     }
