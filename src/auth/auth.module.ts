cc8ea6c30894dc4c279bd58d5100a81bfe645c41
rosanaandre <kawaiicri@gmail.com>
2025-10-09 18:58:10 -0500

    Criando a pasta AUTH

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/interfaces/user.entity';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+    imports:[
+        ConfigModule, 
+        PassportModule,
+        JwtModule.registerAsync({
+            imports:[ConfigModule],
+            inject:[ConfigService],
+            useFactory: (cfg: ConfigService) => ({
+                secret: cfg.get<string>('JWT_SECRET'),
+                signOptions: [ expiresIn: cfg.get<string>('JWT_EXPIRES_IN') || '7d' ],
+            }),
+        }),
+        TypeOrmModule.forFeature([UserEntity]),
+    ],
+
+    providers:[AuthService, JwtStrategy, UserService],
+    controllers:[AuthController],
+    exports:[AuthService],
+})
+export class AuthModule {}
